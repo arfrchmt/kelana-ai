cb8072ee750f4b75bfea9ab0524c8c66e9dceb6c
Arif Rachmat <aiepbug@gmail.com>
2026-08-10 17:43:34 +0700

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+destination = input("Destination: ")
+days = int(input("Number of days: "))
+budget = float(input("Budget: $"))
+travel_style = input("Travel Style: ")
+
+print(f"Destination: {destination}")
+print(f"Days: {days}")
+print(f"Budget: ${budget}")
+print(f"Travel Style: {travel_style}")
