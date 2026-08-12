16f0936e9cbcf14be78980881495a9b7fd221055
arfrchmt <arfrchmt@gmail.com>
2026-08-12 11:38:34 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,39 @@ budget = float(input("Budget: "))
 currency  = input("Currency: ")
 travel_month = input("Travel Month: ")
 
+if budget < 1000:
+    category = "Backpacker"
+elif budget < 3000:
+    category = "Standard"
+else:
+    category = "Luxury"
+
+daily_budget = budget/days
+
+recommended_places = [
+    "Tokyo Tower",
+    "Shibuya",
+    "Mount Fuji"
+]
+
+# Loop through the list
+for place in recommended_places:
+    print(f" - {place}")
+
+def calculate_daily_budget(budget, days):
+    return budget/days
+
+def get_trip_category(budget):
+    if budget < 1000:
+        return "Backpacker"
+    elif budget < 3000:
+        return "Standard"
+    else:
+        return "Luxury"
+
+daily = calculate_daily_budget(1500,5)
+category = get_trip_category(1500)
+print(f"{category} · {daily} USD/day")
 
 def print_trip_summary(destination, country, days, budget, currency, travel_month):
     print("------------------------")
@@ -16,5 +49,7 @@ def print_trip_summary(destination, country, days, budget, currency, travel_mont
     print(f"Budget      : {budget}")
     print(f"Currency      : {currency}")
     print(f"Travel_month       : {travel_month}")
+    print(f"Category : {category}")
+    print(f"Daily Budget : {daily_budget} USD/day")
 
 print_trip_summary(destination, country, days, budget, currency, travel_month)
